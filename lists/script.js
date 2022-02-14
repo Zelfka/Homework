eedd1bb7e45651d1f869a9b57ff61949acb0fba7
Zelfka <mach.veronika@gmail.com>
2022-02-14 15:26:06 +0100

    Update script.js

---
+++ lists/script.js
@@ -0,0 +1,10 @@
+let names = ['Petr','Pavel','Franta','Zuzka','Veronika'];
+names.forEach((name) => {
+  $('ul').append(" " + [name])
+});
+let additionalBlock = {
+  title: "Added with javascript",
+  text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+$('body').append($('additionalBlock'),[additionalBlock.title, additionalBlock.text]);
+$('additionalBlock.title').wrap('<h1></h1>')
